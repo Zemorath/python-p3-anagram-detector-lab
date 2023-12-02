eac52388722a6d3f754404a15445aed6242ab72d
Zemorath <trentward10@gmail.com>
2023-12-01 19:54:02 -0600

    Anagram detector lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        matches = []
+        for list_word in anagrams:
+            if sorted(self.word) == sorted(list_word):
+                matches.append(list_word)
+            else:
+                pass
+        return matches
+
+    
